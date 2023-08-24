8d94f081b5318a1c745da0174879a58ede3f8ff6
stla <laurent_step@yahoo.fr>
2023-08-24 06:39:52 +0200

    saveImage3

--- src/Zeta.hs
+++ src/Zeta.hs
@@ -6,11 +6,12 @@ module Zeta
     , funColor2
     , saveImage
     , saveImage2
+    , saveImage3
     ) where
 import Data.Complex ( Complex(..) )
 import Graphics.Image
     ( makeImageR, writeImage, RGB, Image, Pixel(PixelRGB), Pixel(PixelHSI), VU(..), toPixelRGB, ToRGB (toPixelRGB) )
-import ColorMap (colorMap, colorMap2)
+import ColorMap (colorMap, colorMap2, colorMap3)
 import Math.Weierstrass (weierstrassZeta, ellipticInvariants)
 
 xlimitLwr :: Int
@@ -46,13 +47,18 @@ height' = fromIntegral height
 g2g3 :: (Complex Double, Complex Double)
 g2g3 = ellipticInvariants (0.1 :+ 0) (0 :+ 0.1)
 
+g2, g3 :: Complex Double
+g2 = fst g2g3
+g3 = snd g2g3
+
+
 
 colorFun :: (Int, Int) -> Pixel RGB Double
 colorFun (i, j) = 
     let i' = xlimitLwr' + fromIntegral i / width' * (xlimitUpr' - xlimitLwr')
         j' = ylimitLwr' + fromIntegral j / height' * (ylimitUpr' - ylimitLwr')
         z = i' :+ j' 
-        wz = weierstrassZeta z (fst g2g3) (snd g2g3)
+        wz = weierstrassZeta z g2 g3
         (r, g, b) = colorMap wz
     in
     PixelRGB r g b
@@ -62,10 +68,20 @@ colorFun2 (i, j) =
     let i' = xlimitLwr' + fromIntegral i / width' * (xlimitUpr' - xlimitLwr')
         j' = ylimitLwr' + fromIntegral j / height' * (ylimitUpr' - ylimitLwr')
         z = i' :+ j' 
-        wz = weierstrassZeta z (fst g2g3) (snd g2g3)
+        wz = weierstrassZeta z g2 g3
         (h, s, intensity) = colorMap2 wz
     in
     toPixelRGB (PixelHSI h s intensity)
+
+colorFun3 :: Double -> Double -> (Int, Int) -> Pixel RGB Double
+colorFun3 s r (i, j) = 
+    let i' = xlimitLwr' + fromIntegral i / width' * (xlimitUpr' - xlimitLwr')
+        j' = ylimitLwr' + fromIntegral j / height' * (ylimitUpr' - ylimitLwr')
+        z = i' :+ j' 
+        wz = weierstrassZeta z g2 g3
+        (red, green, blue) = colorMap3 wz s r
+    in
+    PixelRGB red green blue
 
 myImage :: ((Int, Int) -> Pixel RGB Double) -> (Int, Int) -> Image VU RGB Double
 myImage thefun (m, n) = makeImageR VU (m, n) thefun
@@ -76,8 +92,14 @@ funColor = myImage colorFun (width, height)
 funColor2 :: Image VU RGB Double
 funColor2 = myImage colorFun2 (height, width)
 
+funColor3 :: Double -> Double -> Image VU RGB Double
+funColor3 s r = myImage (colorFun3 s r) (height, width)
+
 saveImage :: FilePath -> IO ()
 saveImage file = writeImage ("images/" ++ file) funColor
 
 saveImage2 :: FilePath -> IO ()
 saveImage2 file = writeImage ("images/" ++ file) funColor2
+
+saveImage3 :: FilePath -> IO ()
+saveImage3 file = writeImage ("images/" ++ file) (funColor3 80 4)
